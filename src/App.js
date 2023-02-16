41d47d621861df1a923ba5fbfdd6c7b4c495f73f
Zach-Trani <trani.zach@gmail.com>
2023-02-16 18:28:13 -0500

    created components folder, created SaveChanges component and the Save Change button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import SaveChanges from './Components/SaveChanges/SaveChanges';
 
 function App() {
   return (
     <div className="App">
       <div>
         <p className='text-primary'>test</p>
+        <SaveChanges />
       </div>
     </div>
   );
