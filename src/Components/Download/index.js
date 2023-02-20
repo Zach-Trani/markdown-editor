1d6252b2e5c323b9ec297e3b13bf42403c3e6054
Zach-Trani <trani.zach@gmail.com>
2023-02-20 14:12:10 -0500

    fix: convert all references of save changes component to download component

--- src/Components/Download/index.js
+++ src/Components/Download/index.js
@@ -1,15 +1,15 @@
 import clsx from "clsx";
-import styles from "./save-changes.module.scss";
+import styles from "./downloadButton.module.scss";
 
 /**
- * SaveChanges component is a button that is used to save the changes that a user makes to their markdown document.
+ * Download component is a button that is used to save the changes that a user makes to their markdown document.
  *
- * @return {*} 
+ * @return {*}
  */
-function SaveChanges() {
+function Download() {
   return (
     <div className="ms-3 me-3">
-      <button className={clsx("btn d-flex", styles.saveButton)} type="submit">
+      <button className={clsx("btn d-flex", styles.downloadButton)} type="submit">
         <div className="me-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -22,10 +22,10 @@ function SaveChanges() {
             <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z" />
           </svg>
         </div>
-        Save Changes
+        Download
       </button>
     </div>
   );
 }
 
-export default SaveChanges;
+export default Download;
