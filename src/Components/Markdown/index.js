e93363a8e76a028357ebeb9d6e33bdb5b9b40794
Arianna Johnson <ariannajohnson588@gmail.com>
2023-02-20 14:32:27 -0500

    feat: add styles for editor and preview sections

--- src/Components/Markdown/index.js
+++ src/Components/Markdown/index.js
@@ -1,30 +1,54 @@
-import React, { useState } from 'react';
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import clsx from "clsx";
+import styles from "./markdown.module.scss";
 
 /**
  * Markdown component renders the side by side editor and preview windows allowing users to enter input and have it display to the DOM.
  *
- * @return {*} 
+ * @return {*}
  */
 const Markdown = () => {
-    const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
-    const handleChange = (event) => {
-        setInput(event.target.value)
-    };
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
 
-    return (
-        <div className='row'>
-            <div className='col-12 col-sm-6'>
-                <label htmlFor='name'>Markdown</label>
-                <textarea class="form-control" placeholder="Enter text" rows="24" onChange={handleChange}></textarea>
-            </div>
-            <div className='col-12 col-sm-6'>
-                <label htmlFor='name'>Preview</label>
-                <ReactMarkdown children={input}></ReactMarkdown>
-            </div>
-        </div>
-    );
+  return (
+    <div className="row text-light">
+      <div className="col-12 col-sm-6 p-0">
+        <p
+          className={clsx(
+            "ps-3 m-0 d-flex align-items-center",
+            styles.headingText
+          )}
+        >
+          MARKDOWN
+        </p>
+        <textarea
+          className="form-control w-100 bg-dark text-light ps-3 pt-3"
+          placeholder="Enter text"
+          rows="24"
+          onChange={handleChange}
+        ></textarea>
+      </div>
+      <div className="col-12 col-sm-6 p-0 bg-dark text-light">
+        <p
+          className={clsx(
+            "ps-3 m-0 d-flex align-items-center",
+            styles.headingText
+          )}
+        >
+          PREVIEW
+        </p>
+        <ReactMarkdown
+          children={input}
+          className={clsx("ps-3 pt-3", styles.markdownBody)}
+        ></ReactMarkdown>
+      </div>
+    </div>
+  );
 };
 
 export default Markdown;
