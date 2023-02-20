464fa5b73be55574ae036e7596115615f923c77e
Zach-Trani <trani.zach@gmail.com>
2023-02-20 11:27:51 -0500

    feat: add preview label, adjust textarea rows, remove unused imports

--- src/Components/Markdown/index.js
+++ src/Components/Markdown/index.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown'
-import ReactDom from 'react-dom'
-
 
 /**
  * Markdown component renders the side by side editor and preview windows allowing users to enter input and have it display to the DOM.
@@ -13,16 +11,16 @@ const Markdown = () => {
 
     const handleChange = (event) => {
         setInput(event.target.value)
-        console.log(input)
     };
 
     return (
         <div className='row'>
             <div className='col-12 col-sm-6'>
-                <label htmlFor='name'>Markdown Tab</label>
-                <textarea class="form-control" placeholder="Enter text" rows="25" onChange={handleChange}></textarea>
+                <label htmlFor='name'>Markdown</label>
+                <textarea class="form-control" placeholder="Enter text" rows="24" onChange={handleChange}></textarea>
             </div>
             <div className='col-12 col-sm-6'>
+                <label htmlFor='name'>Preview</label>
                 <ReactMarkdown children={input}></ReactMarkdown>
             </div>
         </div>
