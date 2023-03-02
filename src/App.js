2911161cdeca13d914d83a23fdda801f1ef35837
Zach-Trani <trani.zach@gmail.com>
2023-03-02 13:37:57 -0500

    feat: render the home and about pages using browser links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Markdown from "./Components/Markdown";
+import Home from "./Components/Home";
+import About from "./Components/About"
 
 function App() {
   return (
     <div className="App">
       <div>
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
           <Route path="/markdown" element={<Markdown />} />
-          {/* <Route path='/about' element={<About />} /> */}
+          <Route path='/about' element={<About />} />
         </Routes>
       </div>
     </div>
---
+++ src/Components/About/index.js
@@ -0,0 +1,7 @@
+const About = () => {
+    return (
+        <div>About Page Component</div>
+    );
+}
+
+export default About;
