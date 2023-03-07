33fec9d70f693d40c8884b93ea1b80c8e11807d7
Zach-Trani <trani.zach@gmail.com>
2023-03-07 15:33:36 -0500

    feat: add skeleton for bootstrap card

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,12 +1,15 @@
 import NavBar from "../NavBar";
+import ProfileCard from "../ProfileCard";
 
 const About = () => {
-    return (
-        <>
-        <NavBar showIcon={false} />
-        <div>About Page Component</div>
-        </>
-    );
-}
+  return (
+    <>
+      <NavBar showIcon={false} />
+      <div>About Page Component</div>
+      <ProfileCard />
+      <ProfileCard />
+    </>
+  );
+};
 
 export default About;
