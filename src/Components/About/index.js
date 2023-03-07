bae520634b32ab7ab5496d38a357f43c599ace8e
Zach-Trani <trani.zach@gmail.com>
2023-03-07 17:57:20 -0500

    feat: create ProfileCard render

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,13 +1,28 @@
 import NavBar from "../NavBar";
 import ProfileCard from "../ProfileCard";
+import { profilesData } from "../../Data/profilesData";
 
 const About = () => {
   return (
     <>
       <NavBar showIcon={false} />
       <div>About Page Component</div>
-      <ProfileCard />
-      <ProfileCard />
+      <div>
+        {profilesData.map((profile) => {
+          return (
+            <ProfileCard
+              key={profile.id}
+              name={profile.name}
+              heading={profile.heading}
+              description={profile.description}
+              image={profile.image}
+              github={profile.github}
+              linkedin={profile.linkedin}
+              portfolio={profile.portfolio}
+            />
+          );
+        })}
+      </div>
     </>
   );
 };
