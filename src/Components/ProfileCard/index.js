de0fa3e90d47c051b65a5026abe2246905c0015e
Zach-Trani <trani.zach@gmail.com>
2023-03-07 17:58:53 -0500

    feat: create ProfileCard component

--- src/Components/ProfileCard/index.js
+++ src/Components/ProfileCard/index.js
@@ -1,16 +1,26 @@
-import { profileData} from "../../Data/profileData";
+import { GitHubIcon } from "../BootstrapIcons/GitHubIcon";
 
-
-const ProfileCard = () => {
+const ProfileCard = ({
+  name,
+  heading,
+  description,
+  image,
+  github,
+  linkedin,
+  portfolio,
+}) => {
   return (
-<div className="card" style={{"width": "18rem"}}>
-  <img className="card-img-top" src="..." alt="Card image cap"></img>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+    <div className="card" style={{ width: "18rem" }}>
+      <img className="card-img-top" src={image} alt="devloper picture"></img>
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{description}</p>
+
+        <a href={github}>
+          <GitHubIcon />
+        </a>
+      </div>
+    </div>
   );
 };
 
