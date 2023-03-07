92b98809d1f8ea0484025f73b8fa4b4b6baa8618
Zach-Trani <trani.zach@gmail.com>
2023-03-07 17:59:20 -0500

    feat: create data file for developer cards

---
+++ src/Data/profilesData.js
@@ -0,0 +1,30 @@
+import devAri from "../media/dev-ari.jpeg";
+import devZach from "../media/dev-zach.jpeg";
+
+/**
+ * New profile data generated here by creating an object with profile data.
+ *
+ * @return {*}
+ */
+export const profilesData = [
+  {
+    id: 0,
+    name: "Ari",
+    heading: "Developer",
+    description: "Role Here",
+    image: devAri,
+    github: "https://github.com/ariannalillie",
+    linkedin: "linkedin link",
+    portfolio: "portfolio link",
+  },
+  {
+    id: 1,
+    name: "Zach",
+    heading: "Developer",
+    description: "Role Here",
+    image: devZach,
+    github: "https://github.com/Zach-Trani",
+    linkedin: "linkedin link",
+    portfolio: "portfolio link",
+  },
+];
