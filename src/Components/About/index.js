03dfa881186ea95ededa19ae482013eff88ea2a4
Zach-Trani <trani.zach@gmail.com>
2023-04-05 20:53:28 -0400

    feat: render the Info component

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -2,31 +2,39 @@ import NavBar from "../NavBar";
 import ProfileCard from "../ProfileCard";
 import { profilesData } from "../../Data/profilesData";
 import clsx from "clsx";
-import styles from './about.module.scss'
-
+import styles from "./about.module.scss";
+import Info from "../Info";
 
 /**
  * The About component is a page that displays information about the Markdown Project and developers that created it.
  *
- * @return {*} 
+ * @return {*}
  */
 const About = () => {
   return (
     <>
       <NavBar showIcon={false} />
-      <div className={clsx("row justify-content-evenly p-5 h-100 border-top border-light", styles.aboutPage)}>
+      <div
+        className={clsx(
+          "row justify-content-evenly p-5 h-100 border-top border-light",
+          styles.aboutPage
+        )}
+      >
         {profilesData.map((profile) => {
           return (
-            <ProfileCard
-              key={profile.id}
-              name={profile.name}
-              heading={profile.heading}
-              description={profile.description}
-              image={profile.image}
-              github={profile.github}
-              linkedin={profile.linkedin}
-              portfolio={profile.portfolio}
-            />
+            <div>
+              <Info />
+              <ProfileCard
+                key={profile.id}
+                name={profile.name}
+                heading={profile.heading}
+                description={profile.description}
+                image={profile.image}
+                github={profile.github}
+                linkedin={profile.linkedin}
+                portfolio={profile.portfolio}
+              />
+            </div>
           );
         })}
       </div>
---
+++ src/Components/Info/index.js
@@ -0,0 +1,7 @@
+const Info = () => {
+    return (
+        <div>info component</div>
+    )
+};
+
+export default Info;
